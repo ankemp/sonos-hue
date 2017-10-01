aaea14d02262d8dd36beb3222d7db81a21e90e12
Andrew Kemp <andrew.kemp@bynd.com>
2017-10-01 14:28:18 -0400

    Adding albumcolor method Adding supporting dependencies

--- server/sonos-nowplaying.js
+++ server/sonos-nowplaying.js
@@ -1,4 +1,7 @@
 const Sonos = require('Sonos').Sonos
+const download = require('image-downloader');
+const ColorThief = require('color-thief');
+const ct = new ColorThief();
 
 function nowplaying({ ip, port }) {
   return new Promise((Resolve, Reject) => {
@@ -14,6 +17,21 @@ function nowplaying({ ip, port }) {
       }
     });
   });
+}
+
+exports.albumcolor = (device) => {
+  return nowplaying(device)
+    .then(track => {
+      // console.log(track);
+      const { albumArtURL } = track;
+      const dest = `/tmp/albumart-${Date.now()}.jpg`;
+      return download.image({ url: albumArtURL, dest });
+    })
+    .then(({ filename, image }) => {
+      const color = ct.getColor(image);
+      // console.log(color);
+      return color;
+    })
 }
 
 exports.nowplaying = nowplaying
