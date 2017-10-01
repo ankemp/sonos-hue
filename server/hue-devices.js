562c2d6e5c4653c1af33d79dccb993d3429a22d2
Andrew Kemp <andrew.kemp@bynd.com>
2017-10-01 19:33:42 -0400

    Adding methods for controlling hue bulbs

--- server/hue-devices.js
+++ server/hue-devices.js
@@ -1,4 +1,42 @@
 const hue = require("node-hue-api");
+const secrets = require('../config/secrets.json')
+const HueApi = hue.HueApi;
+const lightState = hue.lightState;
+
+let api, state, lightList = [];
+
+function registerServer(bridge) {
+  const { id, ipaddress } = bridge.pop();
+  api = new HueApi(ipaddress, secrets.hueUser);
+  state = lightState.create();
+  return Promise.resolve();
+}
+
+function findLights() {
+  api.lights()
+    .then(lights => {
+      return lights.lights.filter(light => {
+        return (light.type === 'Color light');
+      });
+    })
+    .then(lights => {
+      lightList = lights;
+    });
+}
+
+function getLight(lightName) {
+  return lightList.find(light => {
+    return light.name === lightName;
+  });
+}
+
+function setColor(lightName, rgbColor) {
+  const light = getLight(lightName);
+  return api.setLightState(light.id, state.rgb(...rgbColor))
+}
 
 hue.nupnpSearch()
-  .then(console.log);
+  .then(registerServer)
+  .then(findLights);
+
+exports.setColor = setColor;
