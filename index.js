706a1cf89064d69660a9ad4a7d5b76b8bf2a7edc
Andrew Kemp <andrew.kemp@bynd.com>
2017-10-01 14:30:13 -0400

    Updating index.js with albumcolor method

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const { search, getZoneCoordinator } = require('./server/sonos-devices');
+const { albumcolor } = require('./server/sonos-nowplaying');
 
 const activeZone = 'Living Room';
 
 search()
   .then(devices => getZoneCoordinator(activeZone, devices))
+  .then(coordinator => albumcolor(coordinator))
 
   .catch(err => console.error(err));
