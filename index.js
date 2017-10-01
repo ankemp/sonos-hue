08681c976d75411fa0afda9ae30cfe469b989728
Andrew Kemp <andrew.kemp@bynd.com>
2017-10-01 19:33:48 -0400

    Wiring it up

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const { search, getZoneCoordinator } = require('./server/sonos-devices');
 const { albumcolor } = require('./server/sonos-nowplaying');
+const { setColor } = require('./server/hue-devices')
 
 const activeZone = 'Living Room';
+const activeLight = 'Living Room Accent';
 
 search()
   .then(devices => getZoneCoordinator(activeZone, devices))
   .then(coordinator => albumcolor(coordinator))
+  .then(color => setColor(activeLight, color))
 
   .catch(err => console.error(err));
