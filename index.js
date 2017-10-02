e1fb25a0c6ffd22e72a98931c37039afc1c9c34a
Andrew Kemp <andrew.kemp@bynd.com>
2017-10-01 20:10:23 -0400

    Rewriting index.js

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+const _ = require('lodash');
 const { search, getZoneCoordinator } = require('./server/sonos-devices');
-const { albumcolor } = require('./server/sonos-nowplaying');
+const { nowplaying, albumcolor } = require('./server/sonos-nowplaying');
 const { setColor } = require('./server/hue-devices')
 
 const activeZone = 'Living Room';
 const activeLight = 'Living Room Accent';
+let activeCoordinator, currentTrack;
 
 search()
   .then(devices => getZoneCoordinator(activeZone, devices))
-  .then(coordinator => albumcolor(coordinator))
-  .then(color => setColor(activeLight, color))
+  .then(coordinator => {
+    activeCoordinator = coordinator;
+    return Promise.resolve();
+  })
+  .then(() => {
+    watchMusic();
+
+    setInterval(() => {
+      watchMusic();
+    }, 5000);
+  })
 
   .catch(err => console.error(err));
+
+function watchMusic() {
+  nowplaying(activeCoordinator)
+    .then(track => {
+      if (_.isUndefined(currentTrack) || currentTrack.title !== track.title) {
+        currentTrack = track;
+        // const timeLeft = track.duration - track.position;
+        // setTimeout(() => {
+        //   watchMusic();
+        // }, timeLeft * 1000);
+        return albumcolor(track);
+      }
+      return Promise.resolve(false);
+    })
+    .then(color => {
+      if (color) {
+        setColor(activeLight, color)
+      }
+    })
+
+    .catch(err => console.error(err));
+}
